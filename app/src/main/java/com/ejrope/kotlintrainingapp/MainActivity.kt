5c90200d701b87024967dbe9cdd2abe618041989
ejrope <ejrope@gmail.com>
2017-04-19 10:17:44 +0200

    using kotlin-android-extensions power in main activity

--- app/src/main/java/com/ejrope/kotlintrainingapp/MainActivity.kt
+++ app/src/main/java/com/ejrope/kotlintrainingapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.ejrope.kotlintrainingapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val text = editText.text
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
