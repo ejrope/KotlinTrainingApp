0c4cbaefd18b0359726bfc265081b038d104e07e
ejrope <ejrope@gmail.com>
2017-04-20 12:49:56 +0200

    Extensions. Add parameter with default value to toast function

--- app/src/main/java/com/ejrope/kotlintrainingapp/Extensions.kt
+++ app/src/main/java/com/ejrope/kotlintrainingapp/Extensions.kt
@@ -3,6 +3,6 @@ package com.ejrope.kotlintrainingapp
 import android.content.Context
 import android.widget.Toast
 
-fun Context.toast(text: CharSequence) {
-    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+fun Context.toast(text: CharSequence, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, text, length).show()
 }
