c7b5e154b4bf9442bea93ff3b94c367b6f59c6dc
ejrope <ejrope@gmail.com>
2017-04-20 12:40:07 +0200

    Extensions. Create extensions file with Context toast function

---
+++ app/src/main/java/com/ejrope/kotlintrainingapp/Extensions.kt
@@ -0,0 +1,8 @@
+package com.ejrope.kotlintrainingapp
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast(text: CharSequence) {
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+}
