d0da2a988a247f6be6e30010c5d9a865072d2247
ejrope <ejrope@gmail.com>
2017-04-20 07:46:38 +0200

    Properties. Item get and set

--- app/src/main/java/com/ejrope/kotlintrainingapp/Item.kt
+++ app/src/main/java/com/ejrope/kotlintrainingapp/Item.kt
@@ -3,5 +3,12 @@ package com.ejrope.kotlintrainingapp
 class Item {
 
     var title: String = ""
+        get() {
+            return "title: $field"
+        }
+        set(value) {
+            field = "title: $value"
+        }
+    
     var url: String = ""
 }
