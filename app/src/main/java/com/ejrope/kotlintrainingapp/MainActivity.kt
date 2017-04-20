8294dfc4b09cedd4e236fb3bcfc3d0d19554b1d1
ejrope <ejrope@gmail.com>
2017-04-20 14:27:24 +0200

    Collections. Create listOf items

--- app/src/main/java/com/ejrope/kotlintrainingapp/MainActivity.kt
+++ app/src/main/java/com/ejrope/kotlintrainingapp/MainActivity.kt
@@ -10,12 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val item = Item("title", "url")
-
-        val (title, _) = item
-
-        //lambda
         button.setOnClickListener { toast(editText.text) }
+
+        val items = listOf(Item("title1","url1"), Item("title2","url2"))
     }
 }
