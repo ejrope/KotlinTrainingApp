7d12a7212daa8e9a08d7512f42b3476a652717e2
ejrope <ejrope@gmail.com>
2017-04-21 12:41:09 +0200

    ItemAdapter. add listener constructor parameter as lambda that recieve a Item without return

--- app/src/main/java/com/ejrope/kotlintrainingapp/ItemAdapter.kt
+++ app/src/main/java/com/ejrope/kotlintrainingapp/ItemAdapter.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.view_item.view.*
 
-class ItemAdapter(val items: List<Item>) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+class ItemAdapter(val items: List<Item>, val listener: (Item) -> Unit) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(parent.inflate(R.layout.view_item))
