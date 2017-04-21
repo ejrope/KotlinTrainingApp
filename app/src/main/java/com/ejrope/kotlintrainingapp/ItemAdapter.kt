d8eaccd62870b20165ec2f45ed282dea3a1e30a6
ejrope <ejrope@gmail.com>
2017-04-21 09:02:26 +0200

    override onCreateViewHolder inflating layout and returning ViewHolder

--- app/src/main/java/com/ejrope/kotlintrainingapp/ItemAdapter.kt
+++ app/src/main/java/com/ejrope/kotlintrainingapp/ItemAdapter.kt
@@ -1,12 +1,15 @@
 package com.ejrope.kotlintrainingapp
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 class ItemAdapter(val items: List<Item>) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.view_item, parent, false)
+        return ViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
