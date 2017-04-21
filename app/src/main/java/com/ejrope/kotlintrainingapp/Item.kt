c96f5027c6b6efc4068fa1f1242b1cfd8b7ad012
ejrope <ejrope@gmail.com>
2017-04-21 08:41:43 +0200

    create getItems function

--- app/src/main/java/com/ejrope/kotlintrainingapp/Item.kt
+++ app/src/main/java/com/ejrope/kotlintrainingapp/Item.kt
@@ -1,3 +1,8 @@
 package com.ejrope.kotlintrainingapp
 
+fun getItems():List<Item> {
+    //Generating range and map into a Item list
+    return (1..10).map { Item(it.toLong(), "Title $it", "http://lorempixel.com/400/400/sports/$it/") }
+}
+
 data class Item(val id: Long, val title: String, val url: String)
