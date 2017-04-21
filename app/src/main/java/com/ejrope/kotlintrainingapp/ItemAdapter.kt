3841dae0454ba0bfe5988a4bfb321425c231b7e4
ejrope <ejrope@gmail.com>
2017-04-21 08:18:06 +0200

    create ItemAdapter class with ViewHolder and assign to recycler

---
+++ app/src/main/java/com/ejrope/kotlintrainingapp/ItemAdapter.kt
@@ -0,0 +1,21 @@
+package com.ejrope.kotlintrainingapp
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class ItemAdapter() : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+}
